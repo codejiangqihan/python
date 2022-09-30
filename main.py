35798e056d43cbae5b403e65c44e2598a7bdcb3b
codejiangqihan <107793872+codejiangqihan@users.noreply.github.com>
2022-10-01 06:41:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-#!/usr/bin/python
-import requests
-import bs4
-import tqdm
-a = requests.get("https://pypi.tuna.tsinghua.edu.cn/simple").text
-b = bs4.BeautifulSoup(a, "lxml")
-s = b.find_all("a")
-with open("pypi.txt", "w") as f:
-   for i in tqdm.tqdm(range(len(s)), total=len(s), desc="进度"):
-       f.write(str(s[i].text )+ "\n")
+import gopup as gp
+covid_baidu_df = gp.covid_baidu(indicator="热搜谣言粉碎")
+print(covid_baidu_df)
