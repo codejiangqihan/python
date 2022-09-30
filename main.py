51cef52aac098d59beefee94998c58e69fd1ad40
codejiangqihan <107793872+codejiangqihan@users.noreply.github.com>
2022-09-30 17:55:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hi")
+#!/usr/bin/python
+import requests
+import bs4
+import tqdm
+a = requests.get("https://pypi.tuna.tsinghua.edu.cn/simple").text
+b = bs4.BeautifulSoup(a, "lxml")
+s = b.find_all("a")
+with open("pypi.txt", "w") as f:
+   for i in tqdm.tqdm(range(len(s)), total=len(s), desc="进度"):
+       f.write(str(s[i].text )+ "\n")
